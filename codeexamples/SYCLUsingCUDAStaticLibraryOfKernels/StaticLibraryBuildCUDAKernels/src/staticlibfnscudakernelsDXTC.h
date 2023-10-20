84ab051c0e996965acd68101d79300030cb4bbdb
Illya Rudkin <illya@codeplay.com>
2023-10-20 10:13:38 +0100

    Remove directory containing another SYCL CUDA interop example. It should not be in this branch.

--- codeexamples/SYCLUsingCUDAStaticLibraryOfKernels/StaticLibraryBuildCUDAKernels/src/staticlibfnscudakernelsDXTC.h
+++
@@ -1,7 +0,0 @@
-namespace StaticLibFnsCUDAKernelsDXTC
-{
-    // List of CUDA kernels (via facade wrappers) functions
-    cudaError_t CompressFacade( const int vBlocksPerLaunch, const uint vBlocks, 
-                                const uint *vpPermutations, const uint *vpImage, uint *vpResult, const int vBlockOffset );
-
-} // namespace StaticLibFnsCUDAKernelsDXTC
